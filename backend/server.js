ae8182c84d1dd0f81689cb5bca1b902fd5bc4530
Pratibha <maanupratibha@gmail.com>
2026-05-16 11:35:25 +0530

    added frontend url in cors 2

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5175",
   process.env.CLIENT_URL,
-  "https://resume-builder-f577.vercel.app/"
+  "https://resume-builder-f577.vercel.app"
 ].filter(Boolean);
 
 app.use(
